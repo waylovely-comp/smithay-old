ids1024/smithay

xwayland: handle Client creation

src/xwayland/xserver.rs
@@ -39,7 +39,6 @@
  * STDOUT.
  */
 use std::{
-    cell::RefCell,
     env,
     io::{self, Read},
     os::unix::{
@@ -48,7 +47,7 @@ use std::{
         process::CommandExt,
     },
     process::{ChildStdout, Command, Stdio},
-    rc::Rc,
+    sync::{Arc, Mutex},
 };
 
 use calloop::{
@@ -56,6 +55,7 @@ use calloop::{
     generic::{Fd, Generic},
     Interest, LoopHandle, Mode, Poll, PostAction, Readiness, Token, TokenFactory,
 };
+use wayland_server::{Client, Display, backend::{ClientData, ClientId, DisconnectReason}};
 
 use slog::{error, info, o};
 
@@ -64,7 +64,7 @@ use super::x11_sockets::{prepare_x11_sockets, X11Lock};
 /// The XWayland handle
 #[derive(Debug)]
 pub struct XWayland {
-    inner: Rc<RefCell<Inner>>,
+    inner: Arc<Mutex<Inner>>,
 }
 
 /// Events generated by the XWayland manager
@@ -84,11 +84,7 @@ pub enum XWaylandEvent {
         connection: UnixStream,
 
         /// Wayland client representing XWayland
-        ///
-        /// This should be used to create a Wayland client using [`Display::insert_client`](wayland_server::Display::insert_client).
-        ///
-        /// When the client is disconnected, you should [`shutdown`](XWayland::shutdown) XWayland.
-        client: UnixStream,
+        client: Client,
 
         /// The display number the XWayland server is available at.
         ///
@@ -115,7 +111,7 @@ impl XWayland {
         let log = crate::slog_or_fallback(logger);
         // We don't expect to ever have more than 2 messages in flight, if XWayland got ready and then died right away
         let (sender, channel) = sync_channel(2);
-        let inner = Rc::new(RefCell::new(Inner {
+        let inner = Arc::new(Mutex::new(Inner {
             instance: None,
             sender,
             log: log.new(o!("smithay_module" => "XWayland")),
@@ -131,8 +127,8 @@ impl XWayland {
     /// wayland `Client` for XWayland.
     ///
     /// Does nothing if XWayland is already started or starting.
-    pub fn start<D>(&self, loop_handle: LoopHandle<'_, D>) -> io::Result<()> {
-        launch(&self.inner, loop_handle)
+    pub fn start<D>(&self, loop_handle: LoopHandle<'_, D>, display: &mut Display<D>) -> io::Result<()> {
+        launch(&self.inner, loop_handle, display)
     }
 
     /// Shutdown XWayland
@@ -140,22 +136,22 @@ impl XWayland {
     /// Does nothing if it was not already running, otherwise kills it and you will
     /// later receive a `XWaylandEvent::Exited` event.
     pub fn shutdown(&self) {
-        self.inner.borrow_mut().shutdown();
+        self.inner.lock().unwrap().shutdown();
     }
 }
 
 impl Drop for XWayland {
     fn drop(&mut self) {
-        self.inner.borrow_mut().shutdown();
+        self.inner.lock().unwrap().shutdown();
     }
 }
 
 #[derive(Debug)]
 struct XWaylandInstance {
     display_lock: X11Lock,
-    wayland_client_fd: Option<UnixStream>,
+    wayland_client: Option<Client>,
     wm_fd: Option<UnixStream>,
-    child_stdout: Option<ChildStdout>,
+    child_stdout: ChildStdout,
 }
 
 // Inner implementation of the XWayland manager
@@ -166,11 +162,27 @@ struct Inner {
     log: ::slog::Logger,
 }
 
+struct XWaylandClientData {
+    inner: Arc<Mutex<Inner>>,
+}
+
+impl ClientData for XWaylandClientData {
+    fn initialized(&self, _client_id: ClientId) {}
+    fn disconnected(&self, _client_id: ClientId, _reason: DisconnectReason) {
+        // If we are unable to take a lock we are most likely called during
+        // a shutdown. This will definitely be the case when the compositor exits
+        // and the XWayland instance is dropped.
+        if let Ok(mut guard) = self.inner.try_lock() {
+            guard.shutdown();
+        }
+    }
+}
+
 // Launch an XWayland server
 //
 // Does nothing if there is already a launched instance
-fn launch<D>(inner: &Rc<RefCell<Inner>>, loop_handle: LoopHandle<'_, D>) -> io::Result<()> {
-    let mut guard = inner.borrow_mut();
+fn launch<D>(inner: &Arc<Mutex<Inner>>, loop_handle: LoopHandle<'_, D>, display: &mut Display<D>) -> io::Result<()> {
+    let mut guard = inner.lock().unwrap();
     if guard.instance.is_some() {
         return Ok(());
     }
@@ -184,18 +196,6 @@ fn launch<D>(inner: &Rc<RefCell<Inner>>, loop_handle: LoopHandle<'_, D>) -> io::
 
     // we have now created all the required sockets
 
-    // Setup the associated wayland client to be created in an idle callback, so that we don't need
-    // to access the dispatch_data *right now*
-    let idle_inner = inner.clone();
-    loop_handle.insert_idle(move |_| {
-        let mut guard = idle_inner.borrow_mut();
-        let guard = &mut *guard;
-
-        if let Some(ref mut instance) = guard.instance {
-            instance.wayland_client_fd = Some(wl_me);
-        }
-    });
-
     // all is ready, we can do the fork dance
     let child_stdout = match spawn_xwayland(lock.display(), wl_x11, x_wm_x11, &x_fds) {
         Ok(child_stdout) => child_stdout,
@@ -205,21 +205,22 @@ fn launch<D>(inner: &Rc<RefCell<Inner>>, loop_handle: LoopHandle<'_, D>) -> io::
         }
     };
 
-    let inner = inner.clone();
+    let loop_inner = inner.clone();
     loop_handle.insert_source(
         Generic::new(Fd(child_stdout.as_raw_fd()), Interest::READ, Mode::Level),
         move |_, _, _| {
             // the closure must be called exactly one time, this cannot panic
-            xwayland_ready(&inner);
+            xwayland_ready(&loop_inner);
             Ok(PostAction::Remove)
         },
     )?;
 
+    let client = display.insert_client(wl_me, Arc::new(XWaylandClientData { inner: inner.clone() }))?;
     guard.instance = Some(XWaylandInstance {
         display_lock: lock,
-        wayland_client_fd: None,
+        wayland_client: Some(client),
         wm_fd: Some(x_wm_me),
-        child_stdout: Some(child_stdout),
+        child_stdout,
     });
 
     Ok(())
@@ -292,14 +293,14 @@ impl Inner {
     }
 }
 
-fn xwayland_ready(inner: &Rc<RefCell<Inner>>) {
+fn xwayland_ready(inner: &Arc<Mutex<Inner>>) {
     // Lots of re-borrowing to please the borrow-checker
-    let mut guard = inner.borrow_mut();
+    let mut guard = inner.lock().unwrap();
     let guard = &mut *guard;
     // instance should never be None at this point
     let instance = guard.instance.as_mut().unwrap();
     // neither the child_stdout
-    let child_stdout = instance.child_stdout.as_mut().unwrap();
+    let child_stdout = &mut instance.child_stdout;
 
     // This reads the one byte that is written when sh receives SIGUSR1
     let mut buffer = [0];
@@ -324,7 +325,7 @@ fn xwayland_ready(inner: &Rc<RefCell<Inner>>) {
         // send error occurs if the user dropped the channel... We cannot do much except ignore.
         let _ = guard.sender.send(XWaylandEvent::Ready {
             connection: instance.wm_fd.take().unwrap(), // This is a bug if None
-            client: instance.wayland_client_fd.take().unwrap(),
+            client: instance.wayland_client.take().unwrap(),
             display: instance.display_lock.display(),
         });
     } else {

waylovely-comp/smithay-old

src/xwayland/xserver.rs
@@ -39,7 +39,6 @@
  * STDOUT.
  */
 use std::{
-    cell::RefCell,
     env,
     io::{self, Read},
     os::unix::{
@@ -48,7 +47,7 @@ use std::{
         process::CommandExt,
     },
     process::{ChildStdout, Command, Stdio},
-    rc::Rc,
+    sync::{Arc, Mutex},
 };
 
 use calloop::{
@@ -56,6 +55,7 @@ use calloop::{
     generic::Generic,
     Interest, LoopHandle, Mode,
 };
+use wayland_server::{Client, DisplayHandle, backend::{ClientData, ClientId, DisconnectReason}};
 
 use slog::{error, info, o};
 
@@ -64,7 +64,7 @@ use super::x11_sockets::{prepare_x11_sockets, X11Lock};
 /// The XWayland handle
 #[derive(Debug)]
 pub struct XWayland {
-    inner: Rc<RefCell<Inner>>,
+    inner: Arc<Mutex<Inner>>,
 }
 
 /// Events generated by the XWayland manager
@@ -84,11 +84,7 @@ pub enum XWaylandEvent {
         connection: UnixStream,
 
         /// Wayland client representing XWayland
-        ///
-        /// This should be used to create a Wayland client using [`Display::insert_client`](wayland_server::Display::insert_client).
-        ///
-        /// When the client is disconnected, you should [`shutdown`](XWayland::shutdown) XWayland.
-        client: UnixStream,
+        client: Client,
 
         /// The display number the XWayland server is available at.
         ///
@@ -115,7 +111,7 @@ impl XWayland {
         let log = crate::slog_or_fallback(logger);
         // We don't expect to ever have more than 2 messages in flight, if XWayland got ready and then died right away
         let (sender, channel) = sync_channel(2);
-        let inner = Rc::new(RefCell::new(Inner {
+        let inner = Arc::new(Mutex::new(Inner {
             instance: None,
             sender,
             log: log.new(o!("smithay_module" => "XWayland")),
@@ -131,8 +127,8 @@ impl XWayland {
     /// wayland `Client` for XWayland.
     ///
     /// Does nothing if XWayland is already started or starting.
-    pub fn start<D>(&self, loop_handle: LoopHandle<'_, D>) -> io::Result<()> {
-        launch(&self.inner, loop_handle)
+    pub fn start<D>(&self, loop_handle: LoopHandle<'_, D>, dh: DisplayHandle) -> io::Result<()> {
+        launch(&self.inner, loop_handle, dh)
     }
 
     /// Shutdown XWayland
@@ -140,22 +136,22 @@ impl XWayland {
     /// Does nothing if it was not already running, otherwise kills it and you will
     /// later receive a `XWaylandEvent::Exited` event.
     pub fn shutdown(&self) {
-        self.inner.borrow_mut().shutdown();
+        self.inner.lock().unwrap().shutdown();
     }
 }
 
 impl Drop for XWayland {
     fn drop(&mut self) {
-        self.inner.borrow_mut().shutdown();
+        self.inner.lock().unwrap().shutdown();
     }
 }
 
 #[derive(Debug)]
 struct XWaylandInstance {
     display_lock: X11Lock,
-    wayland_client_fd: Option<UnixStream>,
+    wayland_client: Option<Client>,
     wm_fd: Option<UnixStream>,
-    child_stdout: Option<ChildStdout>,
+    child_stdout: ChildStdout,
 }
 
 // Inner implementation of the XWayland manager
@@ -166,11 +162,27 @@ struct Inner {
     log: ::slog::Logger,
 }
 
+struct XWaylandClientData {
+    inner: Arc<Mutex<Inner>>,
+}
+
+impl ClientData for XWaylandClientData {
+    fn initialized(&self, _client_id: ClientId) {}
+    fn disconnected(&self, _client_id: ClientId, _reason: DisconnectReason) {
+        // If we are unable to take a lock we are most likely called during
+        // a shutdown. This will definitely be the case when the compositor exits
+        // and the XWayland instance is dropped.
+        if let Ok(mut guard) = self.inner.try_lock() {
+            guard.shutdown();
+        }
+    }
+}
+
 // Launch an XWayland server
 //
 // Does nothing if there is already a launched instance
-fn launch<D>(inner: &Rc<RefCell<Inner>>, loop_handle: LoopHandle<'_, D>) -> io::Result<()> {
-    let mut guard = inner.borrow_mut();
+fn launch<D>(inner: &Arc<Mutex<Inner>>, loop_handle: LoopHandle<'_, D>, mut dh: DisplayHandle) -> io::Result<()> {
+    let mut guard = inner.lock().unwrap();
     if guard.instance.is_some() {
         return Ok(());
     }
@@ -184,18 +196,6 @@ fn launch<D>(inner: &Rc<RefCell<Inner>>, loop_handle: LoopHandle<'_, D>) -> io::
 
     // we have now created all the required sockets
 
-    // Setup the associated wayland client to be created in an idle callback, so that we don't need
-    // to access the dispatch_data *right now*
-    let idle_inner = inner.clone();
-    loop_handle.insert_idle(move |_| {
-        let mut guard = idle_inner.borrow_mut();
-        let guard = &mut *guard;
-
-        if let Some(ref mut instance) = guard.instance {
-            instance.wayland_client_fd = Some(wl_me);
-        }
-    });
-
     // all is ready, we can do the fork dance
     let child_stdout = match spawn_xwayland(lock.display(), wl_x11, x_wm_x11, &x_fds) {
         Ok(child_stdout) => child_stdout,
@@ -205,23 +205,24 @@ fn launch<D>(inner: &Rc<RefCell<Inner>>, loop_handle: LoopHandle<'_, D>) -> io::
         }
     };
 
-    let inner = inner.clone();
+    let loop_inner = inner.clone();
     loop_handle
         .insert_source(
             Generic::<_, io::Error>::new(child_stdout.as_raw_fd(), Interest::READ, Mode::Level),
             move |_, _, _| {
                 // the closure must be called exactly one time, this cannot panic
-                xwayland_ready(&inner);
+                xwayland_ready(&loop_inner);
                 Ok(calloop::PostAction::Remove)
             },
         )
         .map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
 
+    let client = dh.insert_client(wl_me, Arc::new(XWaylandClientData { inner: inner.clone() }))?;
     guard.instance = Some(XWaylandInstance {
         display_lock: lock,
-        wayland_client_fd: None,
+        wayland_client: Some(client),
         wm_fd: Some(x_wm_me),
-        child_stdout: Some(child_stdout),
+        child_stdout,
     });
 
     Ok(())
@@ -304,14 +305,14 @@ impl Inner {
     }
 }
 
-fn xwayland_ready(inner: &Rc<RefCell<Inner>>) {
+fn xwayland_ready(inner: &Arc<Mutex<Inner>>) {
     // Lots of re-borrowing to please the borrow-checker
-    let mut guard = inner.borrow_mut();
+    let mut guard = inner.lock().unwrap();
     let guard = &mut *guard;
     // instance should never be None at this point
     let instance = guard.instance.as_mut().unwrap();
     // neither the child_stdout
-    let child_stdout = instance.child_stdout.as_mut().unwrap();
+    let child_stdout = &mut instance.child_stdout;
 
     // This reads the one byte that is written when sh receives SIGUSR1
     let mut buffer = [0];
@@ -336,7 +337,7 @@ fn xwayland_ready(inner: &Rc<RefCell<Inner>>) {
         // send error occurs if the user dropped the channel... We cannot do much except ignore.
         let _ = guard.sender.send(XWaylandEvent::Ready {
             connection: instance.wm_fd.take().unwrap(), // This is a bug if None
-            client: instance.wayland_client_fd.take().unwrap(),
+            client: instance.wayland_client.take().unwrap(),
             display: instance.display_lock.display(),
         });
     } else {