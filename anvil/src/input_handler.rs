Smithay/smithay

anvil: Adjust winit backend for generic Space

anvil/src/input_handler.rs
@@ -10,10 +10,11 @@ use smithay::{
         self, Axis, AxisSource, Event, InputBackend, InputEvent, KeyState, KeyboardKeyEvent,
         PointerAxisEvent, PointerButtonEvent,
     },
-    desktop::{layer_map_for_output, WindowSurfaceType},
+    desktop::{layer_map_for_output, space::SpaceElement, WindowSurfaceType},
     input::{
         keyboard::{keysyms as xkb, FilterResult, Keysym, ModifiersState},
-        pointer::{AxisFrame, ButtonEvent, MotionEvent},
+        pointer::{AxisFrame, ButtonEvent, MotionEvent, PointerHandler},
+        SeatHandler,
     },
     reexports::wayland_server::{
         protocol::{wl_pointer, wl_surface::WlSurface},
@@ -220,11 +221,12 @@ impl<Backend> AnvilState<Backend> {
                 }
             }
 
-            if let Some((window, _, _)) = self
+            if let Some((window, _)) = self
                 .space
-                .surface_under(self.pointer_location, WindowSurfaceType::ALL)
+                .element_under(self.pointer_location)
+                .map(|(w, p)| (w.clone(), p))
             {
-                self.space.raise_window(&window, true);
+                self.space.raise_element(&window, true);
                 keyboard.set_focus(self, Some(window.toplevel().wl_surface().clone()), serial);
                 return;
             }
@@ -253,7 +255,9 @@ impl<Backend> AnvilState<Backend> {
         }
     }
 
-    pub fn surface_under(&self) -> Option<(WlSurface, Point<i32, Logical>)> {
+    pub fn surface_under<D: SeatHandler + 'static>(
+        &self,
+    ) -> Option<(Box<dyn PointerHandler<D>>, Point<i32, Logical>)> {
         let pos = self.pointer_location;
         let output = self.space.outputs().find(|o| {
             let geometry = self.space.output_geometry(o).unwrap();
@@ -262,13 +266,15 @@ impl<Backend> AnvilState<Backend> {
         let output_geo = self.space.output_geometry(output).unwrap();
         let layers = layer_map_for_output(output);
 
-        let mut under = None;
+        let mut under = Option::<(Box<dyn PointerHandler<D>>, Point<i32, Logical>)>::None;
         if let Some(window) = output
             .user_data()
             .get::<FullscreenSurface>()
             .and_then(|f| f.get())
         {
-            under = window.surface_under(pos - output_geo.loc.to_f64(), WindowSurfaceType::ALL);
+            under = window
+                .surface_under(pos - output_geo.loc.to_f64(), WindowSurfaceType::ALL)
+                .map(|(s, p)| (Box::new(s) as Box<_>, p));
         } else if let Some(layer) = layers
             .layer_under(WlrLayer::Overlay, pos)
             .or_else(|| layers.layer_under(WlrLayer::Top, pos))
@@ -279,9 +285,9 @@ impl<Backend> AnvilState<Backend> {
                     pos - output_geo.loc.to_f64() - layer_loc.to_f64(),
                     WindowSurfaceType::ALL,
                 )
-                .map(|(s, loc)| (s, loc + layer_loc));
-        } else if let Some((_, surface, location)) = self.space.surface_under(pos, WindowSurfaceType::ALL) {
-            under = Some((surface, location));
+                .map(|(s, loc)| (Box::new(s) as Box<_>, loc + layer_loc));
+        } else if let Some((window, location)) = self.space.element_under(pos) {
+            under = Some((Box::new(window.clone()) as Box<_>, location));
         } else if let Some(layer) = layers
             .layer_under(WlrLayer::Bottom, pos)
             .or_else(|| layers.layer_under(WlrLayer::Background, pos))
@@ -292,7 +298,7 @@ impl<Backend> AnvilState<Backend> {
                     pos - output_geo.loc.to_f64() - layer_loc.to_f64(),
                     WindowSurfaceType::ALL,
                 )
-                .map(|(s, loc)| (s, loc + layer_loc));
+                .map(|(s, loc)| (Box::new(s) as Box<_>, loc + layer_loc));
         };
         let input_method = self.seat.input_method().unwrap();
         if let Some((_, point)) = under {

waylovely-comp/smithay-old

anvil/src/input_handler.rs
@@ -10,10 +10,11 @@ use smithay::{
         self, Axis, AxisSource, Event, InputBackend, InputEvent, KeyState, KeyboardKeyEvent,
         PointerAxisEvent, PointerButtonEvent,
     },
-    desktop::{layer_map_for_output, WindowSurfaceType},
+    desktop::{layer_map_for_output, space::SpaceElement, WindowSurfaceType},
     input::{
         keyboard::{keysyms as xkb, FilterResult, Keysym, ModifiersState},
-        pointer::{AxisFrame, ButtonEvent, MotionEvent},
+        pointer::{AxisFrame, ButtonEvent, MotionEvent, PointerHandler},
+        SeatHandler,
     },
     output::Scale,
     reexports::wayland_server::{
@@ -227,11 +228,12 @@ impl<Backend> AnvilState<Backend> {
                 }
             }
 
-            if let Some((window, _, point)) = self
+            if let Some((window, point)) = self
                 .space
-                .surface_under(self.pointer_location, WindowSurfaceType::ALL)
+                .element_under(self.pointer_location)
+                .map(|(w, p)| (w.clone(), p))
             {
-                self.space.raise_window(&window, true);
+                self.space.raise_element(&window, true);
                 input_method.set_point(&point);
                 keyboard.set_focus(self, Some(window.toplevel().wl_surface().clone()), serial);
                 return;
@@ -260,7 +262,9 @@ impl<Backend> AnvilState<Backend> {
         }
     }
 
-    pub fn surface_under(&self) -> Option<(WlSurface, Point<i32, Logical>)> {
+    pub fn surface_under<D: SeatHandler + 'static>(
+        &self,
+    ) -> Option<(Box<dyn PointerHandler<D>>, Point<i32, Logical>)> {
         let pos = self.pointer_location;
         let output = self.space.outputs().find(|o| {
             let geometry = self.space.output_geometry(o).unwrap();
@@ -269,13 +273,15 @@ impl<Backend> AnvilState<Backend> {
         let output_geo = self.space.output_geometry(output).unwrap();
         let layers = layer_map_for_output(output);
 
-        let mut under = None;
+        let mut under = Option::<(Box<dyn PointerHandler<D>>, Point<i32, Logical>)>::None;
         if let Some(window) = output
             .user_data()
             .get::<FullscreenSurface>()
             .and_then(|f| f.get())
         {
-            under = window.surface_under(pos - output_geo.loc.to_f64(), WindowSurfaceType::ALL);
+            under = window
+                .surface_under(pos - output_geo.loc.to_f64(), WindowSurfaceType::ALL)
+                .map(|(s, p)| (Box::new(s) as Box<_>, p));
         } else if let Some(layer) = layers
             .layer_under(WlrLayer::Overlay, pos)
             .or_else(|| layers.layer_under(WlrLayer::Top, pos))
@@ -286,9 +292,9 @@ impl<Backend> AnvilState<Backend> {
                     pos - output_geo.loc.to_f64() - layer_loc.to_f64(),
                     WindowSurfaceType::ALL,
                 )
-                .map(|(s, loc)| (s, loc + layer_loc));
-        } else if let Some((_, surface, location)) = self.space.surface_under(pos, WindowSurfaceType::ALL) {
-            under = Some((surface, location));
+                .map(|(s, loc)| (Box::new(s) as Box<_>, loc + layer_loc));
+        } else if let Some((window, location)) = self.space.element_under(pos) {
+            under = Some((Box::new(window.clone()) as Box<_>, location));
         } else if let Some(layer) = layers
             .layer_under(WlrLayer::Bottom, pos)
             .or_else(|| layers.layer_under(WlrLayer::Background, pos))
@@ -299,7 +305,7 @@ impl<Backend> AnvilState<Backend> {
                     pos - output_geo.loc.to_f64() - layer_loc.to_f64(),
                     WindowSurfaceType::ALL,
                 )
-                .map(|(s, loc)| (s, loc + layer_loc));
+                .map(|(s, loc)| (Box::new(s) as Box<_>, loc + layer_loc));
         };
         under
     }